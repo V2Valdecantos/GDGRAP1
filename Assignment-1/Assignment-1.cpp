e4ee9d411f54f3ff477484ce7e93b066f069ddf8
Vito Valdecantos <andre_valdecantos@dlsu.edu.ph>
2023-05-19 10:22:34 +0800

    Week 2

--- Assignment-1/Assignment-1.cpp
+++ Assignment-1/Assignment-1.cpp
@@ -1,6 +1,9 @@
-#include <GLFW/glfw3.h>
-#include "iostream"
-#include "cmath"
+#define _USE_MATH_DEFINES
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>]
+#include <math.h>
+
 
 int main(void)
 {
@@ -20,7 +23,36 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    gladLoadGL();
 
+    GLfloat vertices[]{
+        0.f, 0.5f, 0.f,
+        -0.5f, -0.5f, 0.f,
+        0.5f, -0.5f, 0.f,
+    };
+
+    GLuint indices[]{
+        0,1,2
+    };
+
+    GLuint VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -28,37 +60,9 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //glBegin(GL_TRIANGLES);
-        //glVertex2f(0.f, 0.45f);
-        //glVertex2f(-0.45f, 0.1f);
-        //glVertex2f(-0.3f, -0.4f);
-        //glEnd();
-
-        //glBegin(GL_TRIANGLES);
-        //glVertex2f(0.f, 0.45f);
-        //glVertex2f(0.45f, 0.1f);
-        //glVertex2f(0.3f, -0.4f);
-        //glEnd();
-
-        //glBegin(GL_TRIANGLES);
-        //glVertex2f(0.f, 0.45f);
-        //glVertex2f(0.3f, -0.4f);
-        //glVertex2f(-0.3f, -0.4f);
-        //glEnd();
-
-        ///*glBegin(GL_POLYGON);
-        //glVertex2f(0.2f, 0.2f);
-        //glVertex2f(-0.2f, 0.2f);
-        //glVertex2f(-0.2f, -0.2f);
-        //glVertex2f(0.2f, -0.2f);
-        //glEnd();*/
-
-        glBegin(GL_POLYGON);
-
-        for (int i = 0; i < 5; i++) {
-            glVertex2f((0.5 * cos((360 / 5) * i * 3.1415 / 180)), (0.5 * sin((360 / 5) * i * 3.1415 / 180)));
-        }
-        glEnd();
+        glBindVertexArray(VAO);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -66,6 +70,9 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
